1f5e498cb599625ddf5959dd0f3bff274db6ee18
DESKTOP-I4CUA5G\rodri <rodo.lopez87@gmail.com>
2018-01-18 01:07:49 -0300

    Se crea solucion WebApiProyect

---
+++ WebApiProject/WebApiProject.Test/BLLTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+
+namespace WebApiProject.Test
+{
+    [TestClass]
+    public class BLLTest
+    {
+        [TestMethod]
+        public void EncriptionWorks()
+        {
+            var plainText = "M1C0ntr4sen4";
+            var encription = new BLL.Encription();
+            var encryptedText = encription.Encrypt(plainText);
+            var decryptedText = encription.Decrypt(encryptedText);
+
+            Assert.AreEqual(plainText, decryptedText);
+        }
+    }
+}
---
+++ WebApiProject/WebApiProject/Controllers/CotizacionController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiProject.Controllers
+{
+    public class CotizacionController : ApiController
+    {
+        PriceManager _priceManager = new PriceManager();
+
+        [Route("api/cotizacion/dolar")]
+        public IHttpActionResult GetDolar()
+        {
+            try
+            {
+                IDivisa divisa = new Dolar();
+                this._priceManager.TipoDivisa = divisa;
+                var result = this._priceManager.GetPrice();
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                throw new HttpResponseException(HttpStatusCode.ServiceUnavailable);
+            }
+        }
+
+        [Route("api/cotizacion/peso")]
+        public IHttpActionResult GetPeso()
+        {
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+
+        [Route("api/cotizacion/real")]
+        public IHttpActionResult GetReal()
+        {
+            throw new HttpResponseException(HttpStatusCode.Unauthorized);
+        }
+    }
+}
