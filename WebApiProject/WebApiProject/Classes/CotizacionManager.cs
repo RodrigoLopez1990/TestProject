77308bc1c5a4e7bbccb761caeb33ad7b1d814cfd
DESKTOP-I4CUA5G\rodri <rodo.lopez87@gmail.com>
2018-01-18 01:36:38 -0300

    Alta de solucion 2

---
+++ WebApiProject/WebApiProject/Classes/CotizacionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiProject
+{
+    public class PriceManager
+    {
+        private IDivisa _tipoDivisa;
+
+        public IDivisa TipoDivisa
+        {
+            set { this._tipoDivisa = value; }
+        }
+
+        public object GetPrice()
+        {
+            return this._tipoDivisa.GetPrice();
+        }
+    }
+}
